b13d9005cb8e08c9026b327c8e11bd31cff25839
Paula <paula.yepesfernandez@gmail.com>
2025-11-24 17:36:45 +0100

    INIT

---
+++ src/Com/QOL/IO.java
@@ -0,0 +1,65 @@
+package Com.QOL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class IO {
+    private Scanner scanner = new Scanner(System.in);
+
+    public int println(String string){
+        try {
+            System.out.println(string);
+            return 1;
+        }catch (Exception e){
+            return 0;
+        }
+    }
+
+    public boolean readBool(){
+        return scanner.nextBoolean();
+    }
+
+    public int print(String string){
+        try {
+            System.out.print(string);
+            return 1;
+        }catch (Exception e){
+            return 0;
+        }
+    }
+
+    public int read(){
+        try {
+            System.in.read();
+            return 1;
+        }catch (Exception e){
+            return 0;
+        }
+    }
+
+    public String readln (){
+        return scanner.nextLine();
+    }
+
+    public float readf (){
+        return scanner.nextFloat();
+    }
+
+    public int readInt(){
+        return scanner.nextInt();
+    }
+
+    public ArrayList<String> ParserNombre(String nombreRaw){
+        String regex = "[\\s]";
+        String[] parsed = nombreRaw.split(regex);
+        ArrayList<String> listaFinal = new ArrayList<>();
+        int index = 0;
+        for (String palabras : parsed){
+            listaFinal.add(parsed[index]);
+            index++;
+
+        }
+        return listaFinal;
+
+    }
+}
